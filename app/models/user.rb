d01381faf7a4137456b9841543a871df18b3035b
Maeva De Campos <kulwilcoox@gmail.com>
2022-11-29 15:05:46 +0100

    model category done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :nickname, :birthday, presence: true
+  validates :nickname, uniqueness: true
+  validates :nickname, length: { minimum: 5 }
+  validates :score, numericality: { only_integer: true }
 end
