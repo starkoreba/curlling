5e1447a7883ea640a15e435602f63175509e67c2
Maeva De Campos <kulwilcoox@gmail.com>
2022-11-29 16:49:36 +0100

    controller activities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'activities/index'
+  get 'activities/show'
+  get 'activites/index'
+  get 'activites/new'
+  get 'activites/create'
+  get 'activites/edit'
+  get 'activites/update'
+  get 'activites/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
