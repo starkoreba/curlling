6b26662b9a576a9a394452d44acf0272a387d19f
Maeva De Campos <kulwilcoox@gmail.com>
2022-12-02 15:10:05 +0100

    patch routes chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :participations, only: :destroy
-  resources :chatroom, only: :show
+  get 'chatroom', to: "chatroom#show"
 
   resources :users, only: %i[show destroy]
   get 'infos', to: "activities#infos"
