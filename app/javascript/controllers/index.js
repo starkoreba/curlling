99b45fab8b7ec6ad82e280688429fafaad32e76f
Rodolphe Desbois <102659395+RodolpheDesbois@users.noreply.github.com>
2022-12-07 12:06:58 +0100

    Update index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,12 @@
 
 import { application } from "./application"
 
+import ChatroomSubscriptionController from "./chatroom_subscription_controller"
+application.register("chatroom-subscription", ChatroomSubscriptionController)
+
+import FlatpickrController from "./flatpickr_controller"
+application.register("flatpickr", FlatpickrController)
+
 import AddBadgeController from "./add_badge_controller"
 application.register("add-badge", AddBadgeController)
 
