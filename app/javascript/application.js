16613ffcd8bd82482620091662de2d5d220e4f79
Maeva De Campos <kulwilcoox@gmail.com>
2022-12-08 14:58:28 +0100

    datetime localtime message

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,5 @@
 // Entry point for the build script in your package.json
 //= require sweetalert2
-//= require local-time
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-
-import LocalTime from "local-time"
-LocalTime.start()
